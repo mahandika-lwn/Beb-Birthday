978e0ac0a2ab2af4086a43bb2a746f3a12eb3443
mahandika-lwn <117810890+mahandika-lwn@users.noreply.github.com>
2023-02-09 06:50:36 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,32 @@
+var config = {
+    texts: [
+        "Halo Irba...",      
+        "Diem-diem bae hehe,",  
+        "Gimana kabarnya? Semoga sehat selalu yap...",
+        "Btw selamat ulang tahun ya bre,",
+        "Meskipun dah H+ sebulan sih wkwk...",
+        "Sengaja, biar beda :v",
+        "Tapi tetep, maaf telat ya...",
+        "Soale agenda Januari kemarin padettt...",
+        "Yang penting sehat terus di sana...",
+        "Semangat kuliahnyaaa!",
+        "Semoga selalu dilancarkan urusannya...",
+        "Semoga dapat bersua lagi di lain kesempatan.",
+        "~temenmu yang prik wkwk",
+    ],
+    
+    imgs: {
+        "Halo Irba...": "./imgs/irba.jpg",
+        
+    },
+    desc: {
+        turn_on: "Nyalakan lampu?",
+        play: "Nyalakan musik?",
+        bannar_coming: "Yuk kita dekorasi!",
+        balloons_flying: "Terbangkan balonnn!",
+        cake_fadein: "Ehem, kue virtual?",
+        light_candle: "Nyalakan lilin ya...",
+        wish_message: "Panjang umurrr!",
+        story: "Secercah pesan :)",
+    }
+};

1353bf0a5ef5594bba871cef3e2a7db8125c453d
Mahandika <117810890+mahandika-lwn@users.noreply.github.com>
2025-09-13 12:50:03 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {
     texts: [
-        "Halo Bubu sayanggg!",      
+        "Halo Bubu sayanggg :)",      
         "Gimana kabarnya? Semoga sehat selalu ya :)",  
         "Anw selamat bertambah umur ya Bubuuu!",
         "Sekalianlah, selamat merintis harapan baru di pertengahan tahun 2026 ini :D",
@@ -33,7 +33,7 @@ var config = {
     ],
     
     imgs: {
-        "Halo Bubu Sayang :)": "./imgs/Nindi.png",
+        "Halo Bubu sayanggg :)": "./imgs/Nindi.png",
         
     },
     desc: {
